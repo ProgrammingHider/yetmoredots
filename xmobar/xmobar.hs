d8540d4dd4cadcd670b1fa9c90baa4fe9f023f5e
ProgrammingHider <mrnebelfugiyt@gmail.com>
2019-04-24 18:34:55 +0200

    added xmobar && xmonad

---
+++ xmobar/xmobar.hs
@@ -0,0 +1,45 @@
+-- This is the New "xmobarrc" Configuation File for the XMonad Window Manager. :
+-- This file is a rewrite of the Original ".Xmobarrc". :
+-- This and the Original ".Xmobarrc" are forks of the file: "xmobar-single.hs" from the repository vicfryzel/xmonad-config.
+-- Credit Goes to vicfryzel for the "xmobar-single.hs" configuation file. :
+-- You May Fork This Repository (https://gitlab.com/ProgrammingHider/monadweed) and edited to your Likeing.
+-- This File and The Repository are licensed under The Unlicense. :
+-- All Forks must mention Me (ProgrammingHider) for the forked version of These Files! :
+-- All Forks must also mention vicfryzel (github.com/vicfryzel) as the Original Creator of These Files! :
+-- Enjoy :)
+
+-- This xmobar config is for a single 4k display (3840x2160) and meant to be
+-- used with the stalonetrayrc-single config.
+--
+-- If you're using a single display with a different resolution, adjust the
+-- position argument below using the given calculation.
+Config {
+    -- Position xmobar along the top, with a stalonetray in the top right.
+    -- Add right padding to xmobar to ensure stalonetray and xmobar don't
+    -- overlap. stalonetrayrc-single is configured for 12 icons, each 23px
+    -- wide. 
+    -- right_padding = num_icons * icon_size
+    -- right_padding = 12 * 23 = 276
+    -- Example: position = TopP 0 276
+    position = TopP 0 144,
+    font = "xft:roboto-9.5",
+    bgColor = "#000000",
+    fgColor = "#ffffff",
+    lowerOnStart = False,
+    overrideRedirect = False,
+    allDesktops = True,
+    persistent = True,
+    commands = [
+        Run Weather "KPAO" ["-t","<tempF>F <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
+        Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+        Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Network "enp5s6" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Date "%a %b %_d %l:%M" "date" 10,
+        Run Com "getMasterVolume" [] "volumelevel" 10,
+        Run StdinReader
+    ],
+    sepChar = "%",
+    alignSep = "}{",
+    template = "%StdinReader% }{ %multicpu%   %memory%   %swap%  %enp5s6%  <fc=#FFFFCC>%date%</fc>"
+}
